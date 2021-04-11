deef35dc86048a4563082ba72d9c15fedfd7c927
IsabelSayagoB <40145915+IsabelSayagoB@users.noreply.github.com>
2021-04-11 01:39:35 -0300

    Funciones EjercicioBeer

--- Ejercicios/beers.js
+++ Ejercicios/beers.js
@@ -19,3 +19,23 @@ const beers = [
     { name: 'Belgian Wit', abv: 5.4, label: 'https://s3.amazonaws.com/brewerydbapi/beer/3CvVQG/upload_xOMnlK-large.png', type: 'Wheat' },
     { name: 'Stolen Fruit', abv: 4.6, label: 'https://s3.amazonaws.com/brewerydbapi/beer/YGT30k/upload_uVCHP7-large.png', type: 'Wheat' },
   ];
+
+
+const newPropertyPrice = beers.map(beer => {
+  beer.name === 'Purple Iris' ? beer.price = 320 : beer.abv < 5.0 ? beer.price = 300 : beer.price = 350
+  return beer
+})
+
+newPropertyPrice.forEach(beer =>{
+  beer.file_name =  beer.label.split('/').pop()
+})
+
+
+
+const orderByProperty = function(arr, property) {
+  return arr.sort((a,b) => a[property] === b[property] ? 0 : a[property] < b[property] ? -1 : 1 )
+
+}
+
+console.log(orderByProperty(newPropertyPrice,'type'))
+
